22c0417aa33dbe66fdd68cca7ac0125a6c7974af
wi1d5ky <poppin.wildsky@gmail.com>
2015-10-13 13:41:21 +0800

    Add function - clear

--- app/src/main/java/cc/wildsky/calculator/MainActivity.java
+++ app/src/main/java/cc/wildsky/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package cc.wildsky.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clear(View v) {
+        TextView t = (TextView)findViewById(R.id.display);
+        t.setText("歸零囉！");
+    }
+
+    public void showNum(Number num) {
+        TextView t = (TextView)findViewById(R.id.display);
+        String s = num.toString();
+        t.setText(s);
     }
 }
